d28f92267042c2ca25596be3cb52dace7cbff06d
Caleb-Omoijuanfo <c.omoijuanfo@gmail.com>
2023-08-09 15:22:53 +0100

    Careers page header

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -1,5 +1,16 @@
+import Image from "next/image";
+
+import styles from "./Careers.module.scss";
+
 const Careers = () => {
-  return <div>This is the careers page</div>;
+  return (
+    <main>
+      <div className={styles.Landing}>
+        <h1 className={styles.HeaderText}>Be part of our Mission</h1>
+        <button className={styles.ActionButton}>Join Our Team</button>
+      </div>
+    </main>
+  );
 };
 
 export default Careers;
