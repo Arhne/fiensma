dc40fc91729fba674853015d23663972681913ad
Caleb-Omoijuanfo <c.omoijuanfo@gmail.com>
2023-09-22 15:20:21 +0100

    apply for job updates

--- src/app/careers/Components/JobCard.tsx
+++ src/app/careers/Components/JobCard.tsx
@@ -1,4 +1,6 @@
 import { IJob } from "@/redux/services/jobApi/interface";
+import { useRouter } from "next/navigation";
+
 import styles from "./JobCard.module.scss";
 
 interface IJobCard {
@@ -6,14 +8,21 @@ interface IJobCard {
 }
 
 const JobCard = ({ data }: IJobCard) => {
+  const router = useRouter();
+
   return (
     <div className={styles.JobCardContainer}>
       <span className={styles.CategoryTag}>{data?.category}</span>
       <div className={styles.TitleActionContainer}>
         <h6 className={styles.Title}>{data?.title}</h6>
-        <span>Apply</span>
+        <span
+          className={styles.Apply}
+          onClick={() => router.push(`/careers/${data._id}`)}
+        >
+          Apply
+        </span>
       </div>
-      <p>We’re looking for an expert ui/ux designer to join our team.</p>
+      <p>We're looking for an expert ui/ux designer to join our team.</p>
       <div className={styles.TypeContainer}>
         <span className={styles.JobType}>{data?.location}</span>
         <span className={styles.ContractType}>{data?.schedule}</span>
