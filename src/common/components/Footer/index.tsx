74b03ca2b7e6925e080ab6163cfd60ce98377bb1
Caleb-Omoijuanfo <c.omoijuanfo@gmail.com>
2023-09-28 09:38:04 +0100

    social links feedback

--- src/common/components/Footer/index.tsx
+++ src/common/components/Footer/index.tsx
@@ -7,6 +7,8 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   const { data: activeQuery } = useFindActiveSetupQuery();
 
+  console.log("activeQuery: ", activeQuery);
+
   const router = useRouter();
 
   return (
@@ -26,7 +28,7 @@ const Footer = () => {
             </a>
           </li>
           <li>
-            <a href="https://instagram.com/ibx__marketplace?igshid=MzRlODBiNWFlZA==">
+            <a href={activeQuery && activeQuery?.data?.socialLinks?.instagram}>
               <img src="https://res.cloudinary.com/dbg2z1svm/image/upload/v1691418196/ibx-website-v2/instagram_lwt3ga.svg" />
             </a>
           </li>
@@ -41,7 +43,7 @@ const Footer = () => {
             </a>
           </li>
           <li>
-            <a href="">
+            <a href={activeQuery && activeQuery?.data?.socialLinks?.linkedin}>
               <img src="https://res.cloudinary.com/dbg2z1svm/image/upload/v1695043080/ibx-website-v2/linkedin_ac5uvc.svg" />
             </a>
           </li>
